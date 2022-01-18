7ccc7b1b69fc94fd09bcd0236a503d42a210e361
Andrew Smith <a.smithb21@gmail.com>
2022-01-18 11:51:52 -0600

    fixed a typo

--- src/schema.js
+++ src/schema.js
@@ -38,7 +38,7 @@ const typeDefs = gql`
     "The track's approximate length to complete, in minutes"
     length: Int @deprecated(reason: "Use durationInSeconds")
     "The track's full duration, in seconds"
-    durationInSecods: Int
+    durationInSeconds: Int
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
@@ -66,7 +66,7 @@ const typeDefs = gql`
     "The module's length in minutes"
     length: Int @deprecated(reason: "Use durationInSeconds")
     "The track's full duration, in seconds"
-    durationInSecods: Int
+    durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
